960b521842787d2845b712a52ca19910f690da1b
eweninja <97000698+mativizo@users.noreply.github.com>
2024-05-22 02:00:01 +0200

    Collections, series, tutorials.

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,37 @@
+const tutorials = [
+    {
+        title: "Poradniki Phasmophobia",
+        videos: [
+            {id: "tVXCNDl2Tew", title: "Phasmophobia Aktualizacja 0.9.0 PRZEGLĄD ZMIAN"},
+            {id: "0WqnDBIbvrM", title: "Phasmophobia Poradnik: Pytania Spirit Box i Lista Pytań"},
+            {id: "rgRAgjO-lbs", title: "6 Różnic EVENT vs HUNT - Jak odróżnić atak od zdarzenia paranormalnego? Phasmophobia Poradnik"},
+            {id: "Vn07nK_GBFE", title: "💰 Zdjęcie DUCHA czy KOŚCI? Phasmophobia Poradnik o Nowych Nagrodach za Zdjęcia 📸"},
+            {id: "zryxVqWBv6k", title: "Przeklęte Przedmioty Sunny Meadows (Nowe Asylum) | Phasmophobia"},
+        ]
+    },
+    {
+        title: "Poradniki ETS 2 | Euro Truck Simulator 2",
+        videos: [
+            {id: "D6wD234bYzk", title: "Brak MAN TG3 TGX u dilera w ETS2? Zrób TO! 🚚" },
+            {id: "DOeq8SSpvdM", title: "Jak Włączyć KONSOLĘ w ETS2? Euro Truck Simulator 2 Poradnik" },
+            {id: "JXE-fRgfPk0", title: "Euro Truck Simulator 2 Śnieg! Mod dodający zimę do ETS 2! Poradnik" },
+        ]
+    }
+]
+
+const timeline = [
+    { title: "400 do MILIONA!", date: "11.01.2024", description: "Wielkie osiągnięcie dla mnie i - mam nadzieję - dla Was także! 400 filmów na kanale i przekroczony milion wyświetleń!"},
+    { title: "Pierwszy 1000!", date: "11.04.2023", description: "Jest nas już 1000! Tysiąc subskrypcji przekroczony!"},
+    { title: "Rekord w 48h!", date: "11.03.2023", description: "Wyświetlenia w ostatnich 48h przekroczyły 20,000!"},
+    { title: "Mała armia!", date: "17.02.2023", description: "Przekraczamy granicę 800 subów! Witamy nowych widzów."},
+    { title: "Mamo, 10k!", date: "16.02.2023", description: "Pierwszy raz przebite 10,000 wyświetleń w 48h!"},
+    { title: "4 stówki!", date: "08.12.2022", description: "Jest nas coraz więcej! 400 osób zasubskrybowało kanał Mativizo na YouTube!"},
+    { title: "Ale łapki!", date: "21.09.2022", description: "Suma łapek na kanale przekroczyła 1000!"},
+    { title: "Pierwsze duchy!", date: "07.07.2022", description: "Powstaje pierwszy, prawdziwy poradnik do Phasmophobii - <a href=\"https://www.youtube.com/watch?v=IGelGHCoSW8\">PRZEKLĘTE PRZEDMIOTY wyjaśnione w 4 minuty | Phasmophobia</a>. Ten film rozpoczął serię poradników!"},
+    { title: "Pierwszy film na kanale!", date: "04.12.2021", description: "Jako pierwszy film na kanale pojawił się <a href=\"https://www.youtube.com/watch?v=fOQaLPZ0gzE\">Jak wykonać podstawowy tuning w Forza Horizon 5? Poradnik dla początkujących</a>. Do tej chwili film zdobył 5000+ wyświetleń."},
+    { title: "Start!", date: "22.11.2021", description: " Założyłem kanał na YouTube - Mativizo. To jeden z wielu kanałów, które założyłem, ale jeden z niewielu, które nadal rozwijam! "},
+];
+
 const youtubeNewest = [
     { id: "2gGisWTgzFM", title: "Kolejny odcinek | Seria Niefortunnych Przekleństw!", description: "Trzeci odcinek z gry Phasmophobia wraz z @Xarieq wpada na kanał! Łapiemy duchy, bez spiny." },
     { id: "O1pCGy6KXhc", title: "Phasmophobia, ale to PRL!", description: "Pierwszy film z serii \"Ale to PRL\". Dajcie znać, jak Wam się podoba taka koncepcja!"},
@@ -10,8 +44,18 @@ const youtubeSeries = [
         description: "Seria ta - we współpracy z @Xarieq - jest oparta na wulgarnym języku i luźnym podejściu do koncepcji szukania duchów. Dwuosobowa ekipa rizzuje zjawy jak trzeba. Dobre do obiadku, ale lepiej nie z rodziną.",
         videos: [
             { id: "P1UQtj8ACno", title: "🤬 JEST TU KTOŚ?! | Phasmophobia SNP #1 @Xarieq" },
+            { id: "vcB4Uu297E4", title: "🤬 Jakaś Pani PŁACZE! | Phasmophobia SNP #2 @Xarieq"},
+            { id: "2gGisWTgzFM", title: "🤬 Czeski DUCH atakuje! | Phasmophobia SNP #3 @Xarieq"},
         ],
         playlist: "https://www.youtube.com/playlist?list=PLsQYoYeIcdaxWgN109CeF1IPr4JlwS4_y"
+    },
+    {
+        title: "Ale to PRL!",
+        description: "Seria \"Ale to PRL\" to koncepcja przerabiania filmu o danej grze na stylizowany na audycję telewizyjną z czasów PRL.",
+        playlist: "#",
+        videos: [
+            { id: "O1pCGy6KXhc", title: "👻 PHASMOPHOBIA, ale to PRL! [Zwiastun Gry]" },
+        ],
     }
 ];
 
@@ -24,6 +68,14 @@ module.exports = function(eleventyConfig) {
     });
     eleventyConfig.addCollection("youtubeSeries", function(collection) {
         return youtubeSeries;
+    });
+
+    eleventyConfig.addCollection("timeline", function(collection) {
+        return timeline;
+    });
+
+    eleventyConfig.addCollection("tutorials", function(collection) {
+        return tutorials;
     });
 
     return {
